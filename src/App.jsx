cd03c8b813937224da06ec27cd21b20c49e7efbb
Houssain Hafed <hafedhoussain@gmail.com>
2024-05-24 15:40:06 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+         <h1 className="text-3xl bg-red-400">
+           Hello world!
+         </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
